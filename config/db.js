6b611d9ed9652088b4c4cdac708c0a309c460fcd
johnkobz14 <johnkobz14@gmail.com>
2021-07-04 21:12:58 +0900

    Jul5_v2

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 
     const pool = new Pool({
-        host: process.env.DBHOST
-        user: process.env.DBUSER,
-        password: process.env.DBPASSWORD,
-        database: process.env.DBASE,
+        host: process.env.REACT_APP_DBHOST,
+        user: process.env.REACT_APP_DBUSER,
+        password: process.env.REACT_APP_DBPASSWORD,
+        database: process.env.REACT_APP_DBASE,
     })
 
     // await pool.connect()
