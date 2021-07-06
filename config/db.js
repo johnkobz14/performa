dcd922711ca14a3a86e28baefad964ce57c0b0fe
johnkobz14 <johnkobz14@gmail.com>
2021-07-07 01:34:29 +0900

    Jul6_v9

--- config/db.js
+++ config/db.js
@@ -1,35 +1,39 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 
-    const pool = new Pool({
-        host: process.env.REACT_APP_HOST,
-        user: process.env.REACT_APP_USER,
-        password: process.env.REACT_APP_PWORD,
-        database: process.env.REACT_APP_DBASE,
-    })
+const pool = new Pool({
+  host: process.env.REACT_APP_HOST,
+  user: process.env.REACT_APP_USER,
+  password: process.env.REACT_APP_PWORD,
+  database: process.env.REACT_APP_DBASE,
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
+});
 
-    // await pool.connect()
-    // await pool.query('select * from businessunit', (err, res) => {
-    //     if (err) {
-    //         console.log('SELECT pool.query():', err)
-    //     }
+// await pool.connect()
+// await pool.query('select * from businessunit', (err, res) => {
+//     if (err) {
+//         console.log('SELECT pool.query():', err)
+//     }
 
-    //     if (res) {
-    //         console.log('SELECT pool.query():', res)
-    //     }
-    // });
+//     if (res) {
+//         console.log('SELECT pool.query():', res)
+//     }
+// });
 
-     pool.connect((err, client, release) => {
-        if (err) {
-          return console.error('Error acquiring client-database', err.stack)
-        }
-        console.log("Connected to Database!");
-        // client.query('SELECT NOW()', (err, result) => {
-        //   release()
-        //   if (err) {
-        //     return console.error('Error executing query', err.stack)
-        //   }
-        //   console.log(result.rows)
-        // })
-      })
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error("Error acquiring client-database", err.stack);
+  }
+  console.log("Connected to Database!");
+  // client.query('SELECT NOW()', (err, result) => {
+  //   release()
+  //   if (err) {
+  //     return console.error('Error executing query', err.stack)
+  //   }
+  //   console.log(result.rows)
+  // })
+});
 
 module.exports = pool;
